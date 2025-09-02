4f23b81b869d9e3e0b60bfdcc12430e61db246f2
itka0526 <itka0526@gmail.com>
2025-09-03 00:56:18 +0300

    final

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
     botLink: import.meta.env.PROD ? "https://t.me/galvpn_bot" : "https://t.me/testgalvpn_bot",
-    BACKEND_ENDPOINT_ADDR: import.meta.env.PROD ? import.meta.env["PROD_URL"] : "http://127.0.0.1:4000", //TODO: Add Proper Production Address
+    BACKEND_ENDPOINT_ADDR: import.meta.env.PROD ? "https://galvpn.xyz" : "http://127.0.0.1:4000", //TODO: Add Proper Production Address
     referralA: 30,
     referralB: 14,
 };
