1dbdd7dde45dbeff4c1c02b0fa89f18b4d250025
itka0526 <itka0526@gmail.com>
2025-09-09 09:05:48 +0300

    final

---
+++ backend/i18n.ts
@@ -0,0 +1,20 @@
+import i18next from "i18next";
+import i18NexFsBackend from "i18next-fs-backend";
+import i18Middleware from "i18next-http-middleware";
+import path from "path";
+import config from "./config";
+
+i18next
+    .use(i18NexFsBackend)
+    .use(i18Middleware.LanguageDetector)
+    .init({
+        fallbackLng: "en",
+        load: "languageOnly",
+        backend: {
+            loadPath: path.join(__dirname, "../locales/{{lng}}/{{ns}}.json"),
+        },
+        preload: ["en", "ru", "mn"],
+        debug: config.nodeEnv === "development",
+    });
+
+export { i18Middleware, i18next };
