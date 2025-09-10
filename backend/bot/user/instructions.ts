1f4cbec38fad3c8ea66ceb05e22a738e3dc48b2f
itka0526 <itka0526@gmail.com>
2025-09-10 03:16:11 +0300

    final

--- backend/bot/user/instructions.ts
+++ backend/bot/user/instructions.ts
@@ -1,15 +1,14 @@
-import { InputMediaVideo } from "grammy/types";
+import { InputFile, InputMediaVideo } from "grammy/types";
 import { pmBot } from "../bot";
 import config from "../../config";
+import path from "path";
 
-const videoBaseURL = config.nodeEnv === "production" ? config.domain : "http://localhost:4000";
-
-const instructionVideos = ["https://" + videoBaseURL + "/public/iOS.mp4"];
+const instructionVideos = [path.resolve(process.cwd(), "/public/iOS.mp4")];
 
 pmBot.command("instructions", async (ctx) => {
     const mediaGroup: InputMediaVideo[] = instructionVideos.map((file) => ({
         type: "video",
-        media: file,
+        media: new InputFile(file),
         caption: `📖 ${file.split("/").pop()} `,
     }));
 
