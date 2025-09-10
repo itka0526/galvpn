5df1a01a011a9c8f0f2189945e81908f85ba8d9b
itka0526 <itka0526@gmail.com>
2025-09-10 03:23:46 +0300

    final

--- backend/bot/user/instructions.ts
+++ backend/bot/user/instructions.ts
@@ -2,13 +2,13 @@ import { InputFile, InputMediaVideo } from "grammy/types";
 import { pmBot } from "../bot";
 import path from "path";
 
-const instructionVideos = [path.resolve(process.cwd(), "/public/iOS.mp4")];
+const instructionVideos = [path.resolve(process.cwd(), "./public/iOS.mp4")];
 
 pmBot.command("instructions", async (ctx) => {
     const mediaGroup: InputMediaVideo[] = instructionVideos.map((file) => ({
         type: "video",
         media: new InputFile(file),
-        caption: `📖 ${file.split("/").pop()} `,
+        caption: `📖 ${file.split("/").pop()?.replace(".mp4", "")} `,
     }));
 
     await ctx.replyWithMediaGroup(mediaGroup);
