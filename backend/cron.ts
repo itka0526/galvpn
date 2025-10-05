a968e2328300ce489b0a7f1c41b5498f69e6cadd
itka0526 <itka0526@gmail.com>
2025-10-06 01:33:21 +0300

    middleware

--- backend/cron.ts
+++ backend/cron.ts
@@ -3,6 +3,6 @@ import { freezeKeys, notifyExpiration } from "./jobs/freezeKeys";
 
 console.log("Cron jobs are running...");
 
-cron.schedule("* 0/6 * * *", freezeKeys);
+cron.schedule("0 0/6 * * *", freezeKeys);
 
 cron.schedule("0 12 * * *", notifyExpiration);
